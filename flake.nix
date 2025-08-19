9f567fe56838e72acee1d240a37f0107636cc251
loner <2788892716@qq.com>
2025-08-19 20:00:56 +0800

    remove flake-parts

--- flake.nix
+++ flake.nix
@@ -3,60 +3,68 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
 
-      perSystem =
-        {
-          self',
-          pkgs,
-          ...
-        }:
+      forAllSystems =
+        f:
+        builtins.listToAttrs (
+          map (system: {
+            name = system;
+            value = f system;
+          }) supportedSystems
+        );
+
+    in
+    {
+      packages = forAllSystems (
+        system:
         let
+          pkgs = import nixpkgs { inherit system; };
           lib = pkgs.lib;
         in
         {
-          packages = {
-            default = self'.packages.dgop;
-            dgop = pkgs.buildGoModule (finalAttrs: {
-              pname = "dgop";
-              version = "0.0.6";
-              src = ./.;
-              vendorHash = "sha256-+5rN3ekzExcnFdxK2xqOzgYiUzxbJtODHGd4HVq6hqk=";
-              ldflags = [
-                "-s"
-                "-w"
-                "-X main.Version=${finalAttrs.version}"
-                "-X main.buildTime=1970-01-01_00:00:00"
-                "-X main.Commit=${finalAttrs.version}"
-              ];
+          dgop = pkgs.buildGoModule (finalAttrs: {
+            pname = "dgop";
+            version = "0.0.6";
+            src = ./.;
+            vendorHash = "sha256-+5rN3ekzExcnFdxK2xqOzgYiUzxbJtODHGd4HVq6hqk=";
 
-              installPhase = ''
-                mkdir -p $out/bin
-                cp $GOPATH/bin/cli $out/bin/dgop
-              '';
+            ldflags = [
+              "-s"
+              "-w"
+              "-X main.Version=${finalAttrs.version}"
+              "-X main.buildTime=1970-01-01_00:00:00"
+              "-X main.Commit=${finalAttrs.version}"
+            ];
 
-              meta = {
-                description = "API & CLI for System Resources";
-                homepage = "https://github.com/AvengeMedia/dgop";
-                mainProgram = "dgop";
-                binaryNativeCode = true;
-                license = lib.licenses.mit;
-                platforms = lib.platforms.unix;
-                maintainers = with lib.maintainers; [ lonerOrz ];
-              };
-            });
-          };
-        };
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $GOPATH/bin/cli $out/bin/dgop
+            '';
+
+            meta = {
+              description = "API & CLI for System Resources";
+              homepage = "https://github.com/AvengeMedia/dgop";
+              mainProgram = "dgop";
+              binaryNativeCode = true;
+              license = lib.licenses.mit;
+              platforms = lib.platforms.unix;
+              maintainers = with lib.maintainers; [ lonerOrz ];
+            };
+          });
+
+          default = self.packages.${system}.dgop;
+        }
+      );
     };
 }
