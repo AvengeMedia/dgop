0ba2c76d797243a49968b25644ea0f4c7bcc693a
bbedward <bbedward@gmail.com>
2025-10-10 21:22:38 -0400

    processes: prefer pss over rss

--- go.mod
+++ go.mod
@@ -54,7 +54,6 @@ require (
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/exp v0.0.0-20251009144603-d2f985daa21b // indirect
-	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
--- gops/processes.go
+++ gops/processes.go
@@ -3,9 +3,13 @@ package gops
 import (
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"os"
 	"reflect"
 	"runtime"
 	"sort"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/AvengeMedia/dgop/models"
@@ -13,6 +17,29 @@ import (
 	"github.com/shirou/gopsutil/v4/mem"
 	"github.com/shirou/gopsutil/v4/process"
 )
+
+func getPssDirty(pid int32) (uint64, error) {
+	smapsRollupPath := fmt.Sprintf("/proc/%d/smaps_rollup", pid)
+	contents, err := os.ReadFile(smapsRollupPath)
+	if err != nil {
+		return 0, err
+	}
+
+	lines := strings.Split(string(contents), "\n")
+	for _, line := range lines {
+		if strings.HasPrefix(line, "Pss_Dirty:") {
+			fields := strings.Fields(line)
+			if len(fields) >= 2 {
+				val, err := strconv.ParseUint(fields[1], 10, 64)
+				if err != nil {
+					return 0, err
+				}
+				return val, nil
+			}
+		}
+	}
+	return 0, fmt.Errorf("Pss_Dirty not found")
+}
 
 func (self *GopsUtil) GetProcesses(sortBy ProcSortBy, limit int, enableCPU bool) (*models.ProcessListResponse, error) {
 	return self.GetProcessesWithCursor(sortBy, limit, enableCPU, "")
@@ -54,7 +81,6 @@ func (self *GopsUtil) GetProcessesWithCursor(sortBy ProcSortBy, limit int, enabl
 	}
 
 	for _, p := range procs {
-		// Get process info
 		name, _ := p.Name()
 		cmdline, _ := p.Cmdline()
 		ppid, _ := p.Ppid()
@@ -62,45 +88,67 @@ func (self *GopsUtil) GetProcessesWithCursor(sortBy ProcSortBy, limit int, enabl
 		times, _ := p.Times()
 		username, _ := p.Username()
 
-		// Calculate current CPU time in seconds (gopsutil already converts to seconds)
 		currentCPUTime := float64(0)
 		if times != nil {
 			currentCPUTime = times.User + times.System
 		}
 
-		// Get CPU percentage only if enabled
 		cpuPercent := 0.0
 		if enableCPU {
 			if cursorData, hasCursor := cursorMap[p.Pid]; hasCursor {
-				// Use cursor data to calculate CPU percentage per core
 				cpuPercent = calculateProcessCPUPercentageWithCursor(cursorData, currentCPUTime, currentTime)
 			} else {
-				// Fallback to gopsutil measurement (normalize to per-CPU like htop)
 				rawCpuPercent, _ := p.CPUPercent()
 				cpuPercent = rawCpuPercent / float64(runtime.NumCPU())
 			}
 		}
 
-		// Calculate memory percentage and KB
+		rssKB := uint64(0)
+		rssPercent := float32(0)
+		pssKB := uint64(0)
+		pssPercent := float32(0)
 		memKB := uint64(0)
 		memPercent := float32(0)
+		memCalc := "rss"
+
 		if memInfo != nil {
-			memKB = memInfo.RSS / 1024
-			memPercent = float32(memInfo.RSS) / float32(totalMem.Total) * 100
+			rssKB = memInfo.RSS / 1024
+			rssPercent = float32(memInfo.RSS) / float32(totalMem.Total) * 100
+
+			memKB = rssKB
+			memPercent = rssPercent
+
+			if rssKB > 20480 {
+				pssDirty, err := getPssDirty(p.Pid)
+				if err == nil && pssDirty > 0 {
+					memMaps, _ := p.MemoryMaps(true)
+					if memMaps != nil && len(*memMaps) > 0 {
+						pssKB = (*memMaps)[0].Pss
+						pssPercent = float32(pssKB*1024) / float32(totalMem.Total) * 100
+					}
+
+					memKB = pssDirty
+					memPercent = float32(memKB*1024) / float32(totalMem.Total) * 100
+					memCalc = "pss_dirty"
+				}
+			}
 		}
 
 		procList = append(procList, &models.ProcessInfo{
-			PID:           p.Pid,
-			PPID:          ppid,
-			CPU:           cpuPercent,
-			PTicks:        currentCPUTime,
-			MemoryPercent: memPercent,
-			MemoryKB:      memKB,
-			PSSKB:         memKB,
-			PSSPercent:    memPercent,
-			Username:      username,
-			Command:       name,
-			FullCommand:   cmdline,
+			PID:               p.Pid,
+			PPID:              ppid,
+			CPU:               cpuPercent,
+			PTicks:            currentCPUTime,
+			MemoryPercent:     memPercent,
+			MemoryKB:          memKB,
+			MemoryCalculation: memCalc,
+			RSSKB:             rssKB,
+			RSSPercent:        rssPercent,
+			PSSKB:             pssKB,
+			PSSPercent:        pssPercent,
+			Username:          username,
+			Command:           name,
+			FullCommand:       cmdline,
 		})
 	}
 
