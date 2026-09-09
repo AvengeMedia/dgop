657e81d2bed6167bad6fd1be950ebe86dd8f4e19
etidona <etidona@me.com>
2026-09-09 12:48:57 +0200

    Add 'wld' prefix to network interface matches for Apple Silicon / Asahi Linux

--- gops/net_linux.go
+++ gops/net_linux.go
@@ -5,7 +5,7 @@ package gops
 import "strings"
 
 func matchesNetworkInterface(name string) bool {
-	prefixes := []string{"wlan", "wlo", "wlp", "eth", "eno", "enp", "ens", "lxc"}
+	prefixes := []string{"wlan", "wlo", "wlp", "eth", "eno", "enp", "ens", "lxc", "wld"}
 	for _, prefix := range prefixes {
 		if strings.HasPrefix(name, prefix) {
 			return true
